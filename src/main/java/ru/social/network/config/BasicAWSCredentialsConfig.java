25e33ec8d7fb944a007e77cd3c102aad0fd203e0
lomovskoy <lomovskoy.kirill@yandex.ru>
2021-02-17 22:12:06 +0300

    #51 BasicAWSCredentialsConfig add log

--- src/main/java/ru/social/network/config/BasicAWSCredentialsConfig.java
+++ src/main/java/ru/social/network/config/BasicAWSCredentialsConfig.java
@@ -5,18 +5,21 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BasicAWSCredentialsConfig {
-
+    private static final Logger LOG = LoggerFactory.getLogger(BasicAWSCredentialsConfig.class);
     private final String accessKey;
     private final String secretKey;
 
     public BasicAWSCredentialsConfig(@Value("${aws.access.key}") String accessKey,
                                      @Value("${aws.secret.key}") String secretKey) {
+        LOG.info("AWSCredentialsConfig: accessKey={} secretKey{}", accessKey, secretKey);
         this.accessKey = accessKey;
         this.secretKey = secretKey;
     }
